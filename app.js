334dad59bc7fa7670db080025b0d6b0ace134982
Drew LeSueur <drewalex@gmail.com>
2013-06-12 06:57:18 +0000

    start with socket.io

---
+++ app.js
@@ -0,0 +1,10 @@
+var io = require('socket.io').listen(8011);
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
+
+
